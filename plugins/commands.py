9e48802748b961a9c9ece0e98e36c44a57d6c1bb
MS-Mihir-Sen <145531394+MS-Mihir-Sen@users.noreply.github.com>
2023-09-28 19:06:54 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,13 +38,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, temp.U_NAME))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("⭕  ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⭕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton("𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="https://t.me/Pikashow_Movies_Request")
+            InlineKeyboardButton("🎥 ꜱᴇᴀʀᴄʜ 🎥", switch_inline_query_current_chat=''), 
+            InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ 🛠️", url="https://t.me/+z2b3yBUUMf80NWVl")
             ],[      
-            InlineKeyboardButton("𝙷𝙴𝙻𝙿 👁️‍🗨️", callback_data="help"),
-            InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 ℹ️", callback_data="about")
+            InlineKeyboardButton("⁉️ ʜᴇʟᴘ ⁉️", callback_data="help"),
+            InlineKeyboardButton("📝 ᴀʙᴏᴜᴛ 📝", callback_data="about")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                    "🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 😌", url=invite_link.invite_link
                 )
             ]
         ]
@@ -88,13 +88,13 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("⭕  ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⭕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton("𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="https://t.me/Pikashow_Movies_Request")
+            InlineKeyboardButton("🎥 ꜱᴇᴀʀᴄʜ 🎥", switch_inline_query_current_chat=''), 
+            InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ 🛠️", url="https://t.me/+z2b3yBUUMf80NWVl")
             ],[      
-            InlineKeyboardButton("𝙷𝙴𝙻𝙿 👁️‍🗨️", callback_data="help"),
-            InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 ℹ️", callback_data="about")
+            InlineKeyboardButton("⁉️ ʜᴇʟᴘ ⁉️", callback_data="help"),
+            InlineKeyboardButton("📝 ᴀʙᴏᴜᴛ 📝", callback_data="about")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
